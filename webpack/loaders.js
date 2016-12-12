a8574240a1abe51886c6fbd2e833b090d7f3468c
pkurochka <p.kurochka@svitla.com>
2016-12-12 13:09:15 +0200

    - fonts path fixes

--- webpack/loaders.js
+++ webpack/loaders.js
@@ -18,7 +18,7 @@ module.exports = [
     },
     {
         test: /\.font\.(js|json)$/,
-        loader: 'style!raw!string-replace?search=url%5C("%5C/&replace=url("&flags=gm!fontgen'
+        loader: 'style!raw!string-replace?search=url%5C("%5C/&replace=url("./&flags=gm!fontgen?fileName=fonts/[hash]-[fontname][ext]'
     },
     {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
